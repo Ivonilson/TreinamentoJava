622bb95b4471c245cf19e8382a9d043138d3b2be
Ivonilson <ivonilsoncardoso@hotmail.com>
2018-12-28 21:38:47 -0200

    update

--- estruturas_condicionais/Usando_While.java
+++ estruturas_condicionais/Usando_While.java
@@ -15,19 +15,40 @@ class Usando_While {
 		System.out.println("DIGITE A SENHA: ");
 		senha = entrada.next();
 
-		while(usuario.intern() != "IVONILSON" && senha.intern() != "123"){
+		int contador = 3;
+
+		while(usuario.intern() != "IVONILSON" && senha.intern() != "123" && contador >= 2){
 			System.out.println("O USUARIO "+usuario+" NAO TEM PERMISSAO PARA LOGAR. TENTE NOVAMENTE...");
 			
-			System.out.println("DIGITE O USUARIO: ");
-			usuario = entrada.next();
+			contador--;
 
-			System.out.println("DIGITE A SENHA: ");
-			senha = entrada.next();
+				if(contador == 1) {
+
+					System.out.println("ESTA E SUA ULTIMA CHANCE...");
+
+						System.out.println("VOCE TEM MAIS "+contador+" CHANCES...");
+						System.out.println("DIGITE O USUARIO: ");
+						usuario = entrada.next();
+
+						System.out.println("DIGITE A SENHA: ");
+						senha = entrada.next();
+
+					} else {
+
+						System.out.println("VOCE TEM MAIS "+contador+" CHANCES...");
+						System.out.println("DIGITE O USUARIO: ");
+						usuario = entrada.next();
+
+						System.out.println("DIGITE A SENHA: ");
+						senha = entrada.next();
+
+					}
+			}
 
 			if(usuario.intern() == "IVONILSON" && senha.intern() == "123"){
 				System.out.println("USUARIO "+usuario+" LOGADO COM SUCESSO.");
 			}
-		}
+
 
 	}
 
